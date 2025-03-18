92dcd0d2cc0c9c47bd4e3155a5009a6866d96387
Maximusinko <maximilian.bumbera@gmail.com>
2025-03-18 12:27:01 +0100

    len som pridal default route na home

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('home');
+});
+
 Route::get('/registerpage', function () {
     return view('registerpage');
 });
