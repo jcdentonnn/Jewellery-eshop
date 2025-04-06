bad73ad6135fdd7f66a4370f9faa5c7613b0a6b7
Maximusinko <maximilian.bumbera@gmail.com>
2025-04-06 15:39:43 +0200

    kategorie prod. su v osobitej tabuľke, opraveny productcontroller aby sa choval podla toho

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,37 +7,61 @@ class ProductController extends Controller
 {
     public function engagement()
     {
-        $products = Product::where('category', 'engagement')->get();
+        $products = Product::whereIn('id', function($query) {
+            $query->select('productid')
+                ->from('categories')
+                ->where('category', 'engagement');
+        })->get();
         return view('engagement', compact('products'));
     }
 
     public function diamonds()
     {
-        $products = Product::where('category', 'diamonds')->get();
+        $products = Product::whereIn('id', function($query) {
+            $query->select('productid')
+                ->from('categories')
+                ->where('category', 'diamonds');
+        })->get();
         return view('diamonds', compact('products'));
     }
 
     public function precious_stone()
     {
-        $products = Product::where('category', 'precious_stone')->get();
+        $products = Product::whereIn('id', function($query) {
+            $query->select('productid')
+                ->from('categories')
+                ->where('category', 'precious_stone');
+        })->get();
         return view('precious_stone', compact('products'));
     }
 
     public function watches()
     {
-        $products = Product::where('category', 'watches')->get();
+        $products = Product::whereIn('id', function($query) {
+            $query->select('productid')
+                ->from('categories')
+                ->where('category', 'watches');
+        })->get();
         return view('watches', compact('products'));
     }
 
     public function accessories()
     {
-        $products = Product::where('category', 'accessories')->get();
+        $products = Product::whereIn('id', function($query) {
+            $query->select('productid')
+                ->from('categories')
+                ->where('category', 'accessories');
+        })->get();
         return view('accessories', compact('products'));
     }
 
     public function art_of_gift()
     {
-        $products = Product::where('category', 'art_of_gift')->get();
+        $products = Product::whereIn('id', function($query) {
+            $query->select('productid')
+                ->from('categories')
+                ->where('category', 'art_of_gift');
+        })->get();
         return view('art_of_gift', compact('products'));
     }
 }
