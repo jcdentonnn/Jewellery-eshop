b818b16d9dab6daf34b6befdab1a4e06f9f0e90a
Maximusinko <maximilian.bumbera@gmail.com>
2025-04-18 09:40:10 +0200

    Revert "Pridané zobrazenie totalceny, jej uloženie do databázy,"
    This reverts commit db3f902d8b9015acdf15b522cb8754bd8714e178.

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -7,19 +7,16 @@ use Illuminate\Support\Facades\DB;
 
 class CartController extends Controller
 {
-    /* Tato funkcia zobrazi všetky položky, ktoré má používateľ v tom momente v košíku */
     public function showCart()
     {
         $userId = session('user_id');
 
-        /*ak user neni prihlaseny, vyzve ho na prihlasenie, TOTO PREROBIM ESTE)*/
         if (!$userId) {
             return redirect('/loginpage');
         }
 
         $cartId = DB::table('shoppingcarts')->where('userid', $userId)->value('id');
 
-        //joinem všetky cartItems, ktoré majú rovnaké id ako id košíka
         $items = DB::table('cartitems')
             ->where('cartid', $cartId)
             ->join('products', 'cartitems.productid', '=', 'products.id')
@@ -35,8 +32,6 @@ class CartController extends Controller
         return view('shoppingcart', compact('items'));
     }
 
-
-    /*ak uživateľ klikne na "+" v košiku, inkrementujem produkt (reloadnem stranku nech sa to načíta)*/
     public function increment_product_amount(Request $request)
     {
         $cartid = DB::table('shoppingcarts')->where('userid', $request->userid)->value('id');
@@ -49,8 +44,6 @@ class CartController extends Controller
         return redirect('/shoppingcart');
     }
 
-
-    /*ak uživateľ klikne na "-" v košiku, dekrementujem produkt (reloadnem stranku nech sa to načíta)*/
     public function decrement_product_amount(Request $request)
     {
         $cartid = DB::table('shoppingcarts')->where('userid', $request->userid)->value('id');
@@ -60,7 +53,6 @@ class CartController extends Controller
             ->where('productid', $request->productid)
             ->value('amount');
 
-        /*ak je množstvo produktu 0, odstrani sa cely zaznam, inak len dekrementuje*/
         if ($currentAmount > 1)
         {
             DB::table('cartitems')
@@ -80,10 +72,6 @@ class CartController extends Controller
         return redirect('/shoppingcart');
     }
 
-
-    /* uživateľ vie aj prepísať hodnotu množstva produktu ručne (nemusí stále klikať +/-)
-     * skontrolujem či hodnota nie je záporná/0, ak áno, odstránim celý záznam
-    */
     public function update_amount(Request $request)
     {
         $cartid = DB::table('shoppingcarts')->where('userid', $request->userid)->value('id');
@@ -95,7 +83,7 @@ class CartController extends Controller
                 ->where('productid', $request->productid)
                 ->delete();
         }
-        else //ak to nie je záporná hodnota/0, updatnem
+        else
         {
             DB::table('cartitems')
                 ->where('cartid', $cartid)
@@ -105,10 +93,6 @@ class CartController extends Controller
         return redirect('/shoppingcart');
     }
 
-
-    /* Tato funkcia sa vykonava na stranke shippingmethod, uloži typ dopravy, platby a cenu
-     * za produkty do databazy
-     */
     public function saveMethod(Request $request)
     {
         $userId = session('user_id');
@@ -119,27 +103,18 @@ class CartController extends Controller
             ->where('id', $cartId)
             ->update([
                 'delivery' => $request->input('delivery'),
-                'payment' => $request->input('payment'),
-                'itemsprice' => $request->input('itemsprice')
+                'payment' => $request->input('payment')
             ]);
 
         return redirect('/inputaddress');
     }
 
-
-
-    /*
-     * Tato funkcia rieši ukladanie adresy a ceny do databazy, nasledne prekopirovanie
-     * objednavky do orders (orders je zaznam vsetkych objednavok, kosik je len temporary čo
-     * ma uživateľ nahodene v danej chvili)
-     * Nasledne kosik zresetuje (pripravý ho na novú objednávku) a presmeruje ho na "order completed"
-    */
     public function saveAddress(Request $request)
     {
         $userId = session('user_id');
+
         $cartId = DB::table('shoppingcarts')->where('userid', $userId)->value('id');
 
-        //ulozi adresove udaje do shoppingcarts tabuľky
         DB::table('shoppingcarts')->where('id', $cartId)->update([
             'emailadress'  => $request->emailadress,
             'firstname'    => $request->firstname,
@@ -150,14 +125,10 @@ class CartController extends Controller
             'zipcode'      => $request->zipcode,
             'state'        => $request->state,
             'phonenumber'  => $request->phonenumber,
-            'totalprice'   => $request->totalprice,
         ]);
-
 
         $cart = DB::table('shoppingcarts')->where('id', $cartId)->first();
 
-        //po dokonceni objednavky:
-        //urobim zaznam objednavky do orders (pretože kosik sa bude resetovat)
         $orderId = DB::table('orders')->insertGetId([
             'userid'       => $cart->userid,
             'payment'      => $cart->payment,
@@ -171,12 +142,8 @@ class CartController extends Controller
             'zipcode'      => $request->zipcode,
             'state'        => $request->state,
             'phonenumber'  => $request->phonenumber,
-            'itemsprice'  => $cart->itemsprice,
-            'totalprice'  => $request->totalprice,
         ]);
 
-
-        //vezmeme všetky položky tabuľky cartItems a prekopírujeme ich do orderItems
         $cartItems = DB::table('cartitems')->where('cartid', $cartId)->get();
 
         foreach ($cartItems as $item) {
@@ -188,7 +155,7 @@ class CartController extends Controller
             ]);
         }
         
-        //vyresetovanie kosika (zaznam je už v orders) uzivatel je potom pripraveny na nový nakup
+        //vyresetovanie kosika (zaznam je teraz v orders) uzivatel je pripraveny na novy nakup
         DB::table('cartitems')->where('cartid', $cartId)->delete();
 
         DB::table('shoppingcarts')->where('id', $cartId)->update([
@@ -205,7 +172,6 @@ class CartController extends Controller
             'phonenumber'  => null,
         ]);
 
-        //presmerovanie na purchase completed stranku
         return redirect('/purchasecompleted');
     }
 }
