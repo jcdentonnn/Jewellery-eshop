caf0e12ab4e5a0c731562637e22c178c1f8f6e71
Maximusinko <maximilian.bumbera@gmail.com>
2025-04-30 11:43:48 +0200

    admin v katalogu produktov vidi namiesto "pridat do kosika" tlacitka "edit" a "delete"

--- resources/views/includes/product-container.blade.php
+++ resources/views/includes/product-container.blade.php
@@ -5,26 +5,42 @@ Dany content je pouzity v blade.php suboroch {accessories, diamonds, engagement,
 
 <div class="wr">
     <div class="product-container">
+        @php
+            $user = null;
+            if (session('user_id')) {
+                $user = DB::table('users')->find(session('user_id'));
+            }
+        @endphp
+
         @forelse($products as $product)
-            <a href="{{ route('productinfo',$product->id) }}">
-                <div class="product-card">
-                    <div class="product-img">
+            <div class="product-card">
+                <div class="product-img">
+                    <a href="{{ route('productinfo',$product->id) }}">
                         <img src="/images/{{ $product->imagename }}" alt="{{ $product->productname }}">
                         <h4 class="prod-name-card">{{ $product->productname }}</h4>
                         <p class="prod-info-card">{{ $product->productdesc }}</p>
-                    </div>
-                    <div class="purchase">
-                        <span class="prod-price-card">{{ number_format($product->price,2) }}€</span>
+                    </a>
+                </div>
+
+                <div class="purchase">
+                    <span class="prod-price-card">{{ number_format($product->price,2) }}€</span>
+
+                    @if($user && $user->isadmin)
+                        <div class="admin-buttons-prod">
+                            <button class="admin-btn-single edit">Edit</button>
+                            <button class="delete-btn">Delete</button> <!-- Remove the form here -->
+                        </div>
+                    @else
                         <form action="{{ route('cart.add') }}" method="POST" style="display:inline">
                             @csrf
                             <input type="hidden" name="productid" value="{{ $product->id }}">
-                            <input type="hidden" name="type"      value="{{ $product->type }}">
-                            <input type="hidden" name="amount"    value="1">
-                            <button class="prod-button" type="submit">Add to Bag</button>
+                            <input type="hidden" name="type" value="{{ $product->type }}">
+                            <input type="hidden" name="amount" value="1">
+                            <button class="prod-button" type="submit">Add to Bag</button>
                         </form>
-                    </div>
+                    @endif
                 </div>
-            </a>
+            </div>
         @empty
             <p>Sorry, no products were found</p>
         @endforelse
